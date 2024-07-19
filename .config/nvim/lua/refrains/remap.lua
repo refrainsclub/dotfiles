41a5c95baa5f005400ba12f60b1ba4f88cffcee0
refrains <git@refrains.club>
2024-07-20 00:45:31 +1200

    new nvim and tmux config inspired by ThePrimeagen

---
+++ .config/nvim/lua/refrains/remap.lua
@@ -0,0 +1,43 @@
+-- leader key
+vim.g.mapleader = " "
+
+-- netrw
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move and reindent
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- center
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- occurences
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- yank to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- lsp format
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- warnings
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+-- locations
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replace all occurences
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- golang err return
+vim.keymap.set(
+    "n",
+    "<leader>ee",
+    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
